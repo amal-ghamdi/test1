420733985b632fb605f011eee5d9b8710507b31f
amalghamdi <amal.m.alghamdi@gmail.com>
2022-08-09 10:35:39 +0200

    Add sample test file

---
+++ tests/test_dolfin.py
@@ -0,0 +1,11 @@
+import pytest
+
+
+def test_dolfin():
+    import dolfin as dl
+    mesh = dl.UnitSquareMesh(20,20)
+    assert(mesh.num_cells()==800)
+
+def test_dolfin_dummy():
+    assert(True)
+
